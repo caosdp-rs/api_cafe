d2de7c4cb95c3ab0f78f95b050ca7a493bcf1103
caosdprs <caosdp@gmail.com>
2024-03-10 04:00:23 -0300

    ok

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const receitas = require('./receitas.json')
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/receitas', (req, res) => res.json(receitas))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
